e967b00412c26dce36641254280c8e9c7aadc7ac
Flávio Micheletti <flavio.micheletti@midway.com.br>
2020-12-01 22:27:48 -0300

    quatro operações

--- quatro-operacoes/app.js
+++ quatro-operacoes/app.js
@@ -1,36 +1,25 @@
-const express = require('express')
-const operacoes = require('./app.js')
-const app = express();
-const port = 3000
-
-
-
-app.listen(port,()=>{
-    console.log(`servidor rodando na porta ${port}`)
-})
-
-
+//
 // Quatro operações:
-
-    function adicionar (a, b){
-        return a + b
-    }
-
-    function subtrair (a, b){
-        return a - b
-    }
-
-    function multiplicar (a, b) {
-        return a * b
-    }
-
-    function dividir (a, b){
-        return a / b
-    }
-        exports.adicionar = adicionar
-        exports.subtrair = subtrair
-        exports.multiplicar = multiplicar
-        exports.dividir = dividir
+//
 
 
+function adicionar(a, b) {
+    return a + b
+}
 
+function subtrair(a, b) {
+    return a - b
+}
+
+function multiplicar(a, b) {
+    return a * b
+}
+
+function dividir(a, b) {
+    return a / b
+}
+
+exports.adicionar = adicionar
+exports.subtrair = subtrair
+exports.multiplicar = multiplicar
+exports.dividir = dividir
--- quatro-operacoes/terminal.js
+++ quatro-operacoes/terminal.js
@@ -4,19 +4,19 @@ const operacoes = require('./app.js')
 
 let resultado = undefined
 
-resultado = operacoes.adicionar (50, 10)
+resultado = operacoes.adicionar(50, 10)
 msg = 'O resultado de 50 + 10 = ' + resultado
 console.log(msg)
 
-resultado = operacoes.subtrair (140, 70)
+resultado = operacoes.subtrair(140, 70)
 msg = 'O resultado de 140 - 70 = ' + resultado
 console.log(msg)
 
-resultado = operacoes.multiplicar (9, 9)
+resultado = operacoes.multiplicar(9, 9)
 msg = 'O resultado de 9 X 9 é = ' + resultado
 console.log(msg)
 
-resultado = operacoes.dividir (750 , 2)
+resultado = operacoes.dividir(750, 2)
 msg = 'O resultado de 750 / 2 é = ' + resultado
 console.log(msg)
 

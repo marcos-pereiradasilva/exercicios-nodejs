8a9253598365ad5062b347ab36d14d2e1610dce3
Flávio Micheletti <flavio.micheletti@midway.com.br>
2021-01-11 21:33:16 -0300

    maior que dez

---
+++ maior-e-menor-que-dez/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const service = require("./app");
+
+const app = express();
+const port = 3000
+
+//
+// http://localhost:3000/he-maior-que-dez?num=12
+//
+app.get('/he-maior-que-dez?', (req, res) => {
+    res.send({
+        dobro: service.heMaiorQueDez(req.query.num)
+    });
+})
+
+app.listen(port, () => {
+    console.log(`servidor rodando na porta ${port}`)
+})

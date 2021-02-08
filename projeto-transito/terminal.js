a10abb344dd9ec1926af8703d4552894d8aa7d60
marcos-pereiradasilva <marsilvapereira22@gmail.com>
2021-02-08 20:41:03 -0300

    del pasta projetos*

--- projeto-transito/terminal.js
+++
@@ -1,7 +0,0 @@
-const verificarVelocidade = require("./app");
-// console.log(verificarVelocidade);
-
-const velocidade = parseInt(process.argv[2], 10);
-
-console.log(verificarVelocidade(velocidade));
-
--- projeto-transito/tests.js
+++
@@ -1,5 +0,0 @@
-const verificarVelocidade = require("./app");
-
-console.log(verificarVelocidade(70) == true);
-console.log(verificarVelocidade(68) == true);
-console.log(verificarVelocidade(67) == false);
